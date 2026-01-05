c8a0273fe7a065c42daad6d7d9e2227d23f3a98c
yhaeul <haeul1005@naver.com>
2026-01-06 03:30:01 +0900

    feat: implement database schema and core application logic

---
+++ snu_toto/alembic/env.py
@@ -0,0 +1,75 @@
+import asyncio
+import sys
+import os
+
+from logging.config import fileConfig
+
+from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+
+sys.path.append(os.path.join(os.getcwd(), "snu_toto"))
+
+
+# Alembic을 통해 인식돼야 하는 모델
+from app.core.database import Base
+from app.core.config import DB_SETTINGS
+from app.users.models import User, PointHistory
+from app.events.models import Event, EventOption, EventImage
+from app.bets.models import Bet
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+config.set_main_option("sqlalchemy.url", DB_SETTINGS.url)
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def do_run_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_migrations_online():
+    """비동기 방식으로 마이그레이션 실행"""
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+if context.is_offline_mode():
+    # 오프라인 모드 설정 (필요 시)
+    context.configure(
+        url=DB_SETTINGS.url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+else:
+    # 온라인 모드 실행
+    asyncio.run(run_migrations_online())
---
+++ snu_toto/app/users/models.py
@@ -0,0 +1,143 @@
+import enum
+import uuid
+from sqlalchemy import String, Integer, DateTime, Boolean, Enum, ForeignKey, func, CheckConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.core.database import Base
+
+class UserRole(str, enum.Enum):
+    """관리자 여부를 위한 Enum"""
+    USER = "USER"
+    ADMIN = "ADMIN"
+
+class PointReason(str, enum.Enum):
+    """포인트 기록 변경 사유를 위한 Enum"""
+    BET = "BET"
+    WIN = "WIN"
+    LOSE = "LOSE"
+    REFUND = "REFUND"
+    ETC = "ETC"
+
+class User(Base):
+    """유저 테이블"""
+    __tablename__ = "users"
+
+    # 유저 ID
+    user_id: Mapped[str] = mapped_column(
+        String(36), 
+        primary_key=True, 
+        default=lambda: str(uuid.uuid4()),
+    )
+    
+    # 이메일
+    email: Mapped[str] = mapped_column(
+        String(255), 
+        unique=True, 
+        nullable=False, 
+        index=True
+    )
+    
+    # 비밀번호
+    hashed_password: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+    )
+    
+    # 닉네임
+    nickname: Mapped[str] = mapped_column(
+        String(20), 
+        unique=True, 
+        nullable=False
+    )
+    
+    # 포인트
+    points: Mapped[int] = mapped_column(
+        Integer, 
+        default=10000,
+        server_default="10000", 
+        nullable=False
+    )
+    
+    # 계정 생성 시각
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime, 
+        server_default=func.now(), 
+        nullable=False
+    )
+    
+    # 관리자 여부
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole), 
+        default=UserRole.USER, 
+        server_default="USER",
+        nullable=False
+    )
+    
+    # 인증 여부
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, 
+        default=False, 
+        server_default="0",
+        nullable=False,
+    )
+
+    point_histories: Mapped[list["PointHistory"]] = relationship("PointHistory", back_populates="user")
+
+    __table_args__ = (
+        CheckConstraint("points >= 0", name="check_points_positive"),
+        CheckConstraint("CHAR_LENGTH(nickname) >= 2", name="check_nickname_length"),
+    )
+
+class PointHistory(Base):
+    """포인트 기록 테이블"""
+    __tablename__ = "point_history"
+
+    # 기록 ID
+    history_id: Mapped[str] = mapped_column(
+        String(36), 
+        primary_key=True, 
+        default=lambda: str(uuid.uuid4())
+    )
+
+    # 유저 ID
+    user_id: Mapped[str] = mapped_column(
+        String(36), 
+        ForeignKey("users.user_id"), 
+        nullable=False
+    )
+
+    # 베팅 ID
+    bet_id: Mapped[str | None] = mapped_column(
+        String(36), 
+        nullable=True
+    )
+
+    # 변화량
+    change_amount: Mapped[int] = mapped_column(
+        Integer, 
+        nullable=False
+    )
+
+    # 변화 이유
+    reason: Mapped[PointReason] = mapped_column(
+        Enum(PointReason), 
+        default=PointReason.ETC, 
+        server_default="ETC",
+        nullable=False
+    )
+
+    # 변화 시각
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime, 
+        server_default=func.now(), 
+        nullable=False
+    )
+
+    # 최종 잔액
+    points_after: Mapped[int] = mapped_column(
+        Integer, 
+        default=0, 
+        server_default="0",
+        nullable=False
+    ) 
+
+    user: Mapped["User"] = relationship("User", back_populates="point_histories")
