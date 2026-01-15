7fc6b768cc31f96c22ea1428e3735ab371a9686e
Mr_chimp <hyunkang02@gmail.com>
2026-01-16 04:29:07 +0900

    fix: error code

--- snu_toto/app/bets/exceptions.py
+++ snu_toto/app/bets/exceptions.py
@@ -2,20 +2,20 @@ from snu_toto.app.common.exceptions import SnutotoException
 
 class EventNotFoundError(SnutotoException):
     def __init__(self) -> None:
-        super().__init__(status_code=404, error_code="ERR_010", error_msg="EVENT NOT FOUND")
+        super().__init__(status_code=404, error_code="ERR_009", error_msg="EVENT NOT FOUND")
 
 class InsufficientBalanceError(SnutotoException):
     def __init__(self) -> None:
-        super().__init__(status_code=400, error_code="ERR_011", error_msg="INSUFFICIENT BALANCE")
+        super().__init__(status_code=400, error_code="ERR_038", error_msg="INSUFFICIENT BALANCE")
 
 class OptionNotFoundError(SnutotoException):
     def __init__(self) -> None:
-        super().__init__(status_code=404, error_code="ERR_012", error_msg="OPTION NOT FOUND")
+        super().__init__(status_code=404, error_code="ERR_039", error_msg="OPTION NOT FOUND")
 
 class EventNotOpenError(SnutotoException):
     def __init__(self) -> None:
-        super().__init__(status_code=409, error_code="ERR_013", error_msg="EVENT NOT OPEN")
+        super().__init__(status_code=409, error_code="ERR_040", error_msg="EVENT NOT OPEN")
 
 class DuplicateBetError(SnutotoException):
     def __init__(self) -> None:
-        super().__init__(status_code=409, error_code="ERR_014", error_msg="DUPLICATE BET")
+        super().__init__(status_code=409, error_code="ERR_041", error_msg="DUPLICATE BET")
